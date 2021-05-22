7835701899d21e161243d881d5a85bd524494496
unknown <spacedawn96@gmail.com>
2021-05-22 09:52:25 +0900

    add emotion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,21 @@
+import { css } from '@emotion/react';
+
+const titleStyle = css({
+  boxSizing: 'border-box',
+  width: 300,
+  height: 200,
+  color: 'red',
+});
+
+const subtitleStyle = css`
+  box-sizing: border-box;
+  width: 100px;
+  height: 60px;
+`;
+
 export default function Home() {
   return (
-    <div>
+    <div css={titleStyle}>
       <div>hello</div>
     </div>
   );
