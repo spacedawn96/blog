1bce543b1bcdef3bd2010efa0df7521dd330222c
spacedawn96 <spacedawn96@gmail.com>
2021-05-26 21:29:13 +0900

    init

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { css } from '@emotion/react';
+
+const HeaderBlock = css({
+
+});
+
+export default function Header() {
+  return (
+    <div css={HeaderBlock}>
+      <div>snippet</div>
+    </div>
+  );
+}
