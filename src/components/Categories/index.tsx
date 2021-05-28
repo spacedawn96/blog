7a283214efbcaa37de80ea2e44ebfbebf73ce64c
unknown <spacedawn96@gmail.com>
2021-05-28 15:47:30 +0900

    init'

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,68 +1,32 @@
 import { css } from '@emotion/react';
 import { SelectContext } from '../../lib/context';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 export type CategoriesProps = {};
 
 export default function Categories({}: CategoriesProps) {
-  const {
-    state: { filterList, activeFilter },
-    dispatch,
-  } = useContext(SelectContext) as any;
-
-  const onFilterChange = (filter: any) => {
-    if (filter === 'ALL') {
-      if (activeFilter?.length === filterList?.length) {
-        dispatch({ activeFilter: [] });
-      } else {
-        dispatch({
-          activeFilter: filterList.map((filter: any) => (filter as any).name),
-        });
-      }
-    } else {
-      if (activeFilter?.includes(filter)) {
-        const filterIndex = activeFilter.indexOf(filter);
-        const newFilter = [...activeFilter];
-        newFilter.splice(filterIndex, 1);
-        dispatch({ activeFilter: newFilter });
-      } else {
-        dispatch({ activeFilter: [...activeFilter, filter] });
-      }
-    }
-  };
-
   return (
-    <main>
-      <h1>React Input Checkbox – Themed</h1>
-
-      <fieldset>
-        <legend>Playground – What is awesome?</legend>
-
-        <form style={{ display: 'flex', flexDirection: 'column', padding: '10px 10px' }}>
-          <div style={{ padding: '10px 0' }}>
-            <label htmlFor="myInput">All</label>
-            <input
-              id="myInput"
-              type="checkbox"
-              onClick={() => onFilterChange('ALL')}
-              checked={activeFilter.length === filterList.length}
-            />
-          </div>
-
-          {filterList.map((filter: any) => (
-            <div style={{ padding: '10px 0' }}>
-              <label htmlFor={filter.id}>{filter.name}</label>
-              <input
-                id={filter.id}
-                type="checkbox"
-                checked={activeFilter.includes(filter.name)}
-                onClick={() => onFilterChange(filter.name)}
-              />
-            </div>
-          ))}
-        </form>
-      </fieldset>
-    </main>
+    <section css={CategoriesStyle}>
+      <span>Javascript</span>
+      <span>Typescript</span>
+      <span>React</span>
+      <span>Node js</span>
+      <span>Database</span>
+    </section>
   );
 }
-const CategoriesStyle = css({});
+const CategoriesStyle = css({
+  display: 'flex',
+  flexDirection: 'column',
+  color: '#292929',
+  textTransform: 'uppercase',
+  fontFamily: '"Aeonik-Bold","Helvetica","Arial",sans-serif',
+  fontSize: '5rem',
+  animationDelay: '.2s',
+  animationDuration: '.8s',
+  animationName: 'bottomToTop',
+  animationFillMode: 'both',
+  animationTimingFunction: 'ease',
+  marginBottom: '8px',
+  fontWeight: 600,
+});
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,41 +1,45 @@
 import { css } from '@emotion/react';
 
+export default function Header() {
+  return (
+    <div css={HeaderBlock}>
+      <nav css={Nav}>
+        <section css={NavList}>
+          <span>POSTS</span>
+          <span>ABOUT</span>
+          <span>IMG</span>
+          <span>GITHUB</span>
+          <span>THINK</span>
+        </section>
+      </nav>
+    </div>
+  );
+}
+
 const HeaderBlock = css({
   display: 'flex',
-  border: '1px solid red',
-  height: '200px',
+  height: '10rem',
   alignItems: 'center',
 });
 
 const Nav = css({
-  border: '1px solid blue',
   width: '100%',
 });
 
 const NavList = css({
   display: 'flex',
-
-  li: {
-    ':nth-child(1)': { flexGrow: 1 },
-    ':nth-child(2)': { flexGrow: 1 },
-    ':nth-child(3)': { flexGrow: 3 },
-    ':nth-child(4)': { flexGrow: 3 },
-    ':nth-child(5)': { flexGrow: 3 },
+  span: {
+    ':nth-of-type(1)': { flexGrow: 0.3 },
+    ':nth-of-type(2)': { flexGrow: 3 },
+    ':nth-of-type(3)': { flexGrow: 1 },
+    ':nth-of-type(4)': { flexGrow: 0.3 },
+    ':nth-of-type(5)': { flexGrow: 1 },
   },
-});
 
-export default function Header() {
-  return (
-    <div css={HeaderBlock}>
-      <nav css={Nav}>
-        <ul css={NavList}>
-          <li>POSTS</li>
-          <li>ABOUT</li>
-          <li>IMG</li>
-          <li>GITHUB</li>
-          <li>THINK</li>
-        </ul>
-      </nav>
-    </div>
-  );
-}
+  color: '#292929',
+  textTransform: 'uppercase',
+  fontFamily: '"Aeonik-Bold","Helvetica","Arial",sans-serif',
+  transition: 'all .3s ease',
+  fontWeight: 600,
+  fontSize: '1rem',
+});
